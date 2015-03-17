6cf4eb31fc240bc689ea22dd8564a1076249bf34
Deano <dnoparker@gmail.com>
2015-03-17 17:17:37 +0000

    no message

---
+++ Main.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+	public GameObject obsPrefab;
+	public GameObject cylinder;
+	public GameObject cylinder2;
+	public bool pinchActive = false;  // this exists to distinguish between using two fingers or one
+	public static float score = 0;
+	public float highscore = 0;
+	public TextMesh scoreText1;
+	public TextMesh scoreText2;
+
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+		InvokeRepeating("CreateObstacle", 1f, 4f); // Call this method every 4 seconds
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		highscore = PlayerPrefs.GetFloat ("Highscore");  
+		if (highscore < score) {
+			PlayerPrefs.SetFloat ("Highscore", score);
+			scoreText2.text = score.ToString();
+		}
+		scoreText1.text = score.ToString ();
+		scoreText2.text = highscore.ToString();
+		
+	}
+
+	public void Rotate(Vector2 Delta) {
+		if (pinchActive == false) {
+			this.transform.Rotate (Vector3.up * Delta.x / 5);  // rotate up based on fingers movement on the X axis
+		}
+	}
+
+	public void PinchIn() {
+		pinchActive = true;
+		cylinder.transform.localPosition += new Vector3 (0.5F*Time.deltaTime,0,0);
+		cylinder2.transform.localPosition -= new Vector3 (0.5F*Time.deltaTime,0,0);
+	}
+
+	public void PinchOut() {
+		pinchActive = true;
+		cylinder.transform.localPosition -= new Vector3 (0.5F*Time.deltaTime,0,0);
+		cylinder2.transform.localPosition += new Vector3 (0.5F*Time.deltaTime,0,0);
+	}
+
+	public void PinchEnd() {
+		pinchActive = false;
+	}
+	
+	void CreateObstacle()
+	{
+		float ObsSelect = Random.Range (1, 6);
+		Debug.Log (ObsSelect);
+		Instantiate(obsPrefab); // spawn an obstacle 
+	}
+}
---
+++ Obs.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obs : MonoBehaviour {
+	public float posY;
+
+	// Use this for initialization
+	void Start () {
+		float scaleRangeX = Random.Range(0.5F,3F);
+		float scaleRangeY = Random.Range(0.5F,1F);
+		transform.localScale = new Vector3(scaleRangeX, scaleRangeY, 1);
+		float posX = Random.Range (-2.3F, 2.3F);
+		posY = transform.position.y;
+		float posZ = transform.position.z;
+		transform.position = new Vector3 (posX,posY,posZ);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		posY = transform.position.y;
+		float speed = 1.5F * Time.deltaTime;
+		transform.Translate(Vector3.down * speed);
+
+		if (posY < -5) {
+			Main.score = Main.score +1;
+			Destroy(gameObject);
+			speed = speed + 0.1F;
+		}
+	}
+
+
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.tag == "Cylinder")
+		{
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+}
